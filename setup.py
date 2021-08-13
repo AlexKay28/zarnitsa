26f720e8734b2ed9e35e320e9b1251f4fe85d569
AlexKay28 <a.kaygorodow@ipavlov.ai>
2021-08-13 22:36:37 +0300

    added spacy lib

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'numpy',        # numpy==1.18.5
-    'pandas',       # pandas==1.1.5
-    "scikit-learn", # scikit-learn==0.24.1
-    "nlpaug",       # nlpaug==1.1.3
-    "wget",         # wget==3.2
+    "numpy",  # numpy==1.18.5
+    "pandas",  # pandas==1.1.5
+    "scikit-learn",  # scikit-learn==0.24.1
+    "nlpaug",  # nlpaug==1.1.3
+    "wget",  # wget==3.2
+    "spacy",  # spacy==3.0.5
 ]
 
 setup(
-    name='zarnitsa',
+    name="zarnitsa",
     packages=find_packages(),
-    package_data={'zarnitsa': ['internal_data/*']},
-    description='zarnitsa package for data augmentation',
-    version='0.0.2',
-    url='https://github.com/AlexKay28/zarnitsa',
-    author='Kaigorodov Alexander',
-    author_email='kaygorodo2305@gmail.com',
-    download_url='https://pypi.org/project/zarnitsa/',
+    package_data={"zarnitsa": ["internal_data/*"]},
+    description="zarnitsa package for data augmentation",
+    version="0.0.2",
+    url="https://github.com/AlexKay28/zarnitsa",
+    author="Kaigorodov Alexander",
+    author_email="kaygorodo2305@gmail.com",
+    download_url="https://pypi.org/project/zarnitsa/",
     install_requires=install_requires,
-    keywords=['augmentation', 'NLP', 'distributions'],
-    include_package_data=True
+    keywords=["augmentation", "NLP", "distributions"],
+    include_package_data=True,
 )
