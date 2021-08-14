34f38d57fa403a02df81e69bc63c43806ce504bb
AlexKay28 <a.kaygorodow@ipavlov.ai>
2021-08-14 20:46:26 +0300

    added specified number of aug examples + refactoring

--- zarnitsa/DataAugmenterInternally.py
+++ zarnitsa/DataAugmenterInternally.py
@@ -12,52 +12,36 @@ class DataAugmenterInternally(AbstractDataAugmenter):
     def __init__(self, n_jobs=1):
         self.n_jobs = n_jobs
 
-    def augment_dataframe(
-        self,
-        df: pd.DataFrame,
-        aug_type="normal",
-        freq=0.2,
-        return_only_aug=False,
-    ) -> pd.DataFrame:
+    def augment_dataframe(self, df: pd.DataFrame, **kwargs) -> pd.DataFrame:
         """Augment dataframe data. Pandas dataframe"""
         augment_column_method = {
             "normal": self.augment_column_norm,
             "uniform": self.augment_column_uniform,
             "permutations": self.augment_column_permut,
         }
-        not_to_aug, to_aug = self._prepare_data_to_aug(df, freq=freq)
+        not_to_aug, to_aug = self._prepare_data_to_aug(df, freq=kwargs["freq"])
+        kwargs["freq"] = 1.0
         for col in df.columns:
-            to_aug[col] = augment_column_method[aug_type](
-                to_aug[col], freq=1.0
-            )
+            to_aug[col] = augment_column_method[aug_type](to_aug[col], **kwargs)
         return to_aug if return_only_aug else pd.concat([not_to_aug, to_aug])
 
-    def augment_column(
-        self,
-        col: pd.Series,
-        aug_type="normal",
-        freq=0.2,
-        return_only_aug=False,
-    ) -> pd.Series:
+    def augment_column(self, col: pd.Series, aug_type="permutations", **kwargs) -> pd.Series:
         """Augment Serial data. Pandas column"""
         augment_column_method = {
             "normal": self.augment_column_norm,
             "uniform": self.augment_column_uniform,
             "permutations": self.augment_column_permut,
         }
-        not_to_aug, to_aug = self._prepare_data_to_aug(col, freq=freq)
-        to_aug = augment_column_method[aug_type](to_aug, freq=1.0)
+        to_aug = augment_column_method[aug_type](col, **kwargs)
         return to_aug if return_only_aug else pd.concat([not_to_aug, to_aug])
 
-    def _prepare_data_to_aug(
-        self, data, freq=0.2
-    ) -> Tuple[pd.Series, pd.Series]:
-        """Get part of data. Not augment all of it excep case freq=1.0"""
-        data = (
-            pd.Series(data)
-            if type(data) is not pd.Series and type(data) is not pd.DataFrame
-            else data
-        )
+    def _prepare_data_to_aug(self, data, freq=0.2) -> Tuple[pd.Series, pd.Series]:
+        """
+        Get part of data. Not augment all of it excep case freq=1.0
+        params: data: iterable data or pd.Series object
+        params: freq: part of the data which will be the base for augmentation
+        """
+        data = pd.Series(data) if not isinstance(data, pd.Series) else data
         if freq < 1:
             not_to_aug, to_aug = train_test_split(data, test_size=freq)
             return not_to_aug, to_aug
@@ -67,30 +51,53 @@ class DataAugmenterInternally(AbstractDataAugmenter):
             return data, data.sample(0)
 
     def augment_column_permut(
-        self, col: pd.Series, freq=0.2, return_only_aug=False
+        self, col: pd.Series, n_to_aug=None, freq=0.2, return_only_aug=False
     ) -> pd.Series:
-        """Augment column data using permutations. Pandas column"""
+        """
+        Augment column data using permutations. Pandas column
+        params: col: pandas series object
+        params: n_to_aug: defined number of augmented examples
+        params: freq: part of the data which will be the base for augmentation
+        params: return_only_aug: ask return only augmented data
+        """
         not_to_aug, to_aug = self._prepare_data_to_aug(col, freq=freq)
         indices_to_permute = to_aug.index
         to_aug = to_aug.sample(frac=1.0)
         to_aug.index = indices_to_permute
+        if n_to_aug:
+            n_to_aug = n_to_aug if n_to_aug < to_aug.shape[0] else to_aug.shape[0]
+            to_aug = to_aug.sample(n_to_aug)
         return to_aug if return_only_aug else pd.concat([not_to_aug, to_aug])
 
     def augment_column_norm(
-        self, col: pd.Series, freq=0.2, return_only_aug=False
+        self, col: pd.Series, n_to_aug=None, freq=0.2, return_only_aug=False
     ) -> pd.Series:
-        """Augment column data using normal distribution. Pandas column"""
+        """
+        Augment column data using normal distribution. Pandas column
+        params: col: pandas series object
+        params: n_to_aug: defined number of augmented examples
+        params: freq: part of the data which will be the base for augmentation
+        params: return_only_aug: ask return only augmented data
+        """
         not_to_aug, to_aug = self._prepare_data_to_aug(col, freq=freq)
         column_std = col.std()
-        to_aug = to_aug.apply(
-            lambda value: np.random.normal(value, column_std)
-        )
+        to_aug = to_aug.apply(lambda value: np.random.normal(value, column_std))
+        if n_to_aug:
+            n_to_aug = n_to_aug if n_to_aug < to_aug.shape[0] else to_aug.shape[0]
+            to_aug = to_aug.sample(n_to_aug)
         return to_aug if return_only_aug else pd.concat([not_to_aug, to_aug])
 
     def augment_column_uniform(
-        self, col: pd.Series, freq=0.2, n_sigm=3, return_only_aug=False
+        self, col: pd.Series, n_to_aug=None, freq=0.2, n_sigm=3, return_only_aug=False
     ) -> pd.Series:
-        """Augment column data using uniform distribution. Pandas column"""
+        """
+        Augment column data using uniform distribution. Pandas column
+        params: col: pandas series object
+        params: n_to_aug: defined number of augmented examples
+        params: freq: part of the data which will be the base for augmentation
+        params: n_sigm: the size of std and terms of sigma value
+        params: return_only_aug: ask return only augmented data
+        """
         not_to_aug, to_aug = self._prepare_data_to_aug(col, freq=freq)
         column_std = col.std()
         to_aug = to_aug.apply(
@@ -98,4 +105,7 @@ class DataAugmenterInternally(AbstractDataAugmenter):
                 value - n_sigm * column_std, value + n_sigm * column_std
             )
         )
+        if n_to_aug:
+            n_to_aug = n_to_aug if n_to_aug < to_aug.shape[0] else to_aug.shape[0]
+            to_aug = to_aug.sample(n_to_aug)
         return to_aug if return_only_aug else pd.concat([not_to_aug, to_aug])
