5170f9506353e4dfa5f56168d6c9e000126a9253
AlexKay28 <kaygorodo2305@gmail.com>
2021-09-21 15:47:10 +0300

    change setup version

--- setup.py
+++ setup.py
@@ -12,9 +12,8 @@ install_requires = [
 setup(
     name="zarnitsa",
     packages=find_packages(),
-    package_data={"zarnitsa": ["internal_data/*"]},
     description="zarnitsa package for data augmentation",
-    version="0.0.13",
+    version="0.0.15",
     url="https://github.com/AlexKay28/zarnitsa",
     author="Kaigorodov Alexander",
     author_email="kaygorodo2305@gmail.com",
