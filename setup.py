84a548a7c6d74a73e6ed38b471b5e1dcd7f171ea
AlexKay28 <kaygorodo2305@gmail.com>
2021-09-22 12:51:17 +0300

    delete hard dependencies restriction

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    "numpy==1.18.5",
-    "pandas==1.1.5",
+    "numpy>=1.16.1",
+    "pandas>=1.1.5",
     "scikit-learn==0.24.1",
     "nlpaug==1.1.3",
-    "wget==3.2",
-    "spacy==3.0.5",
+    "wget>=3.2",
+    "spacy>=3.0.5",
 ]
 
 setup(
     name="zarnitsa",
-    version="0.0.17",
+    version="0.0.18",
     description="zarnitsa package for data augmentation",
     author="Kaigorodov Alexander",
     author_email="kaygorodo2305@gmail.com",
--- update_package.sh
+++ update_package.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+python3 -m pip install --upgrade build
+python3 -m build
+
+python3 -m pip install --upgrade twine
+
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/* --verbose
